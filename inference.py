3c32202cedee0f688dda6243de72326c1d99a57c
linjunhao <linjunhao@bytedance.com>
2022-08-24 14:19:10 +0800

    update inference file

---
+++ inference.py
@@ -0,0 +1,516 @@
+import numpy as np
+import torch
+from models import build_model
+import argparse
+import random
+import os
+import cv2
+import numpy as np
+from util import box_ops
+from torch import nn
+from tqdm import tqdm
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+from PIL import Image
+import datasets.transforms as T
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('Deformable DETR Detector',
+                                     add_help=False)
+
+    
+    parser.add_argument('--save_dir', default="./inference_results", type=str)
+    parser.add_argument('--video_dir', default="/opt/tiger/prt_det/CVA-Net/miccai_buv/rawframes/malignant/1dc9ca2f1748c2ec", type=str)
+
+    
+    parser.add_argument('--num_global_frames', default=3, type=int)
+    parser.add_argument('--self_attn', default=True, action='store_true')
+    parser.add_argument('--shuffled_aug',
+                        default='centerCrop',
+                        type=str,
+                        help="Name of the shuffled video augmentation to use")
+    parser.add_argument('--umap', default=False, action='store_true')
+    parser.add_argument('--umap_loss', default=False, action='store_true')
+    parser.add_argument('--lr', default=5e-5, type=float)
+    parser.add_argument('--lr_backbone_names',
+                        default=["backbone.0"],
+                        type=str,
+                        nargs='+')
+    parser.add_argument('--lr_backbone', default=2e-5, type=float)
+    parser.add_argument('--test_size', default=512, type=int)
+
+    parser.add_argument('--lr_linear_proj_names',
+                        default=['reference_points', 'sampling_offsets'],
+                        type=str,
+                        nargs='+')
+    parser.add_argument('--lr_linear_proj_mult', default=0.1, type=float)
+    parser.add_argument('--batch_size', default=1, type=int)
+    parser.add_argument('--weight_decay', default=1e-4, type=float)
+    parser.add_argument('--epochs', default=50, type=int)
+    parser.add_argument('--lr_drop', default=40, type=int)
+    parser.add_argument('--lr_drop_epochs', default=None, type=int, nargs='+')
+    parser.add_argument('--clip_max_norm',
+                        default=0.1,
+                        type=float,
+                        help='gradient clipping max norm')
+    parser.add_argument('--sgd', action='store_true')
+
+    # Variants of Deformable DETR
+    parser.add_argument('--with_box_refine',
+                        default=False,
+                        action='store_true')
+    parser.add_argument('--two_stage', default=False, action='store_true')
+
+    # Model parameters
+    parser.add_argument(
+        '--frozen_weights',
+        type=str,
+        default=None,
+        help=
+        "Path to the pretrained model. If set, only the mask head will be trained"
+    )
+
+    # * Backbone
+    parser.add_argument('--backbone',
+                        default='resnet50',
+                        type=str,
+                        help="Name of the convolutional backbone to use")
+    parser.add_argument(
+        '--dilation',
+        action='store_true',
+        help=
+        "If true, we replace stride with dilation in the last convolutional block (DC5)"
+    )
+    parser.add_argument(
+        '--position_embedding',
+        default='sine',
+        type=str,
+        choices=('sine', 'learned'),
+        help="Type of positional embedding to use on top of the image features"
+    )
+    parser.add_argument('--position_embedding_scale',
+                        default=2 * np.pi,
+                        type=float,
+                        help="position / size * scale")
+    parser.add_argument('--num_feature_levels',
+                        default=4,
+                        type=int,
+                        help='number of feature levels')
+
+    # * Transformer
+    parser.add_argument('--enc_layers',
+                        default=6,
+                        type=int,
+                        help="Number of encoding layers in the transformer")
+    parser.add_argument('--dec_layers',
+                        default=6,
+                        type=int,
+                        help="Number of decoding layers in the transformer")
+    parser.add_argument(
+        '--dim_feedforward',
+        default=1024,
+        type=int,
+        help=
+        "Intermediate size of the feedforward layers in the transformer blocks"
+    )
+    parser.add_argument(
+        '--hidden_dim',
+        default=256,
+        type=int,
+        help="Size of the embeddings (dimension of the transformer)")
+    parser.add_argument('--dropout',
+                        default=0.1,
+                        type=float,
+                        help="Dropout applied in the transformer")
+
+    parser.add_argument(
+        '--nheads',
+        default=8,
+        type=int,
+        help="Number of attention heads inside the transformer's attentions")
+    parser.add_argument('--num_queries',
+                        default=300,
+                        type=int,
+                        help="Number of query slots")
+    parser.add_argument('--dec_n_points', default=6, type=int)
+    parser.add_argument('--enc_n_points', default=6, type=int)
+
+    # * Segmentation
+    parser.add_argument('--masks',
+                        action='store_true',
+                        help="Train segmentation head if the flag is provided")
+
+    # Loss
+    parser.add_argument(
+        '--no_aux_loss',
+        dest='aux_loss',
+        action='store_false',
+        help="Disables auxiliary decoding losses (loss at each layer)")
+
+    # * Matcher
+    parser.add_argument('--set_cost_class',
+                        default=2,
+                        type=float,
+                        help="Class coefficient in the matching cost")
+    parser.add_argument('--set_cost_bbox',
+                        default=5,
+                        type=float,
+                        help="L1 box coefficient in the matching cost")
+    parser.add_argument('--set_cost_giou',
+                        default=2,
+                        type=float,
+                        help="giou box coefficient in the matching cost")
+
+    # * Loss coefficients
+    parser.add_argument('--mask_loss_coef', default=1, type=float)
+    parser.add_argument('--dice_loss_coef', default=1, type=float)
+    parser.add_argument('--cls_loss_coef', default=2, type=float)
+    parser.add_argument('--bbox_loss_coef', default=5, type=float)
+    parser.add_argument('--giou_loss_coef', default=2, type=float)
+    parser.add_argument('--focal_alpha', default=0.25, type=float)
+
+    # dataset parameters
+    parser.add_argument('--dataset_file', default='coco')
+    # parser.add_argument('--coco_path', default='./data/coco', type=str)
+    # data_mode = ['15frames', '30frames', 'every5frames', 'every10frames']
+    parser.add_argument('--data_mode', default='15frames', type=str)
+    parser.add_argument('--data_coco_lite_path', default='./miccai_buv', type=str)
+
+    parser.add_argument('--coco_panoptic_path', type=str)
+    parser.add_argument('--remove_difficult', action='store_true')
+    parser.add_argument('--dist_url',
+                        default='env://',
+                        help='url used to set up distributed training')
+    parser.add_argument('--output_dir',
+                        default='',
+                        help='path where to save, empty for no saving')
+    parser.add_argument('--device',
+                        default='cuda',
+                        help='device to use for training / testing')
+    parser.add_argument('--seed', default=42, type=int)
+    parser.add_argument('--ckpt', default='/opt/tiger/prt_det/CVA-Net/run/self_attn_exp8/checkpoint0042.pth', help='resume from checkpoint')
+    parser.add_argument('--start_epoch',
+                        default=0,
+                        type=int,
+                        metavar='N',
+                        help='start epoch')
+    parser.add_argument('--eval', action='store_true')
+    parser.add_argument('--num_workers', default=3, type=int)
+    parser.add_argument('--cache_mode',
+                        default=False,
+                        action='store_true',
+                        help='whether to cache images on memory')
+
+    return parser
+
+class PostProcess(nn.Module):
+    """ This module converts the model's output into the format expected by the coco api"""
+
+    @torch.no_grad()
+    def forward(self, outputs, target_sizes):
+        """ Perform the computation
+        Parameters:
+            outputs: raw outputs of the model
+            target_sizes: tensor of dimension [batch_size x 2] containing the size of each images of the batch
+                          For evaluation, this must be the original image size (before any data augmentation)
+                          For visualization, this should be the image size after data augment, but before padding
+        """
+        out_logits, out_bbox = outputs['pred_logits'], outputs['pred_boxes']
+
+        assert len(out_logits) == len(target_sizes)
+        assert target_sizes.shape[1] == 2
+
+        prob = out_logits.sigmoid()
+        topk_values, topk_indexes = torch.topk(prob.view(out_logits.shape[0], -1), 100, dim=1)
+        scores = topk_values
+        topk_boxes = topk_indexes // out_logits.shape[2]
+        labels = topk_indexes % out_logits.shape[2]
+        boxes = box_ops.box_cxcywh_to_xyxy(out_bbox)
+        boxes = torch.gather(boxes, 1, topk_boxes.unsqueeze(-1).repeat(1,1,4))
+
+        # and from relative [0, 1] to absolute [0, height] coordinates
+        img_h, img_w = target_sizes.unbind(1)
+        scale_fct = torch.stack([img_w, img_h, img_w, img_h], dim=1)
+        boxes = boxes * scale_fct[:, None, :]
+
+        results = [{'scores': s, 'labels': l, 'boxes': b} for s, l, b in zip(scores, labels, boxes)]
+
+        return results
+
+def preproc(img, input_size, swap=(2, 0, 1)):
+    if len(img.shape) == 3:
+        padded_img = np.ones((input_size[0], input_size[1], 3), dtype=np.uint8) * 114
+    else:
+        padded_img = np.ones(input_size, dtype=np.uint8) * 114
+
+    r = min(input_size[0] / img.shape[0], input_size[1] / img.shape[1])
+    resized_img = cv2.resize(
+        img,
+        (int(img.shape[1] * r), int(img.shape[0] * r)),
+        interpolation=cv2.INTER_LINEAR,
+    ).astype(np.uint8)
+    padded_img[: int(img.shape[0] * r), : int(img.shape[1] * r)] = resized_img
+
+    padded_img = padded_img.transpose(swap)
+    padded_img = np.ascontiguousarray(padded_img, dtype=np.float32)
+    return padded_img, r
+
+class ValTransform:
+    """
+    Defines the transformations that should be applied to test PIL image
+    for input into the network
+
+    dimension -> tensorize -> color adj
+
+    Arguments:
+        resize (int): input dimension to SSD
+        rgb_means ((int,int,int)): average RGB of the dataset
+            (104,117,123)
+        swap ((int,int,int)): final order of channels
+
+    Returns:
+        transform (transform) : callable transform to be applied to test/val
+        data
+    """
+
+    def __init__(self, swap=(2, 0, 1), legacy=False):
+        self.swap = swap
+        self.legacy = legacy
+
+    # assume input is cv2 img for now
+    def __call__(self, img, input_size):
+        img, _ = preproc(img, input_size, self.swap)
+        if self.legacy:
+            img = img[::-1, :, :].copy()
+            img /= 255.0
+            img -= np.array([0.485, 0.456, 0.406]).reshape(3, 1, 1)
+            img /= np.array([0.229, 0.224, 0.225]).reshape(3, 1, 1)
+        return img, np.zeros((1, 5))
+
+
+
+def getVideo_list(video_root_dir):
+    videos = os.listdir(video_root_dir)
+    video_list = dict()
+    for video in videos:
+        video_folder = os.path.join(video_root_dir, video)
+        frames = sorted(os.listdir(video_folder))
+        frames = [os.path.join(video_folder, x) for x in frames if x.endswith(".png")]
+        video_list[video] = frames
+    return video_list
+
+def getFrames(video_root_dir):
+    frms = sorted(os.listdir(video_root_dir))
+    frms = [os.path.join(video_root_dir, x) for x in frms]
+    return frms
+
+
+def load_imgs(frames):
+    image_list = []
+    for frm in frames:
+        image = Image.open(frm).convert("RGB")
+        ori_shape = image.size
+        transform = T.Compose([
+            T.RandomResize([512], max_size=1333),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+        image, _ = transform(image, None)
+        image_list.append(image)
+    return image_list, ori_shape
+
+
+
+
+_COLORS = np.array(
+    [
+        0.000, 0.447, 0.741,
+        0.850, 0.325, 0.098,
+        0.929, 0.694, 0.125,
+        0.494, 0.184, 0.556,
+        0.466, 0.674, 0.188,
+        0.301, 0.745, 0.933,
+        0.635, 0.078, 0.184,
+        0.300, 0.300, 0.300,
+        0.600, 0.600, 0.600,
+        1.000, 0.000, 0.000,
+        1.000, 0.500, 0.000,
+        0.749, 0.749, 0.000,
+        0.000, 1.000, 0.000,
+        0.000, 0.000, 1.000,
+        0.667, 0.000, 1.000,
+        0.333, 0.333, 0.000,
+        0.333, 0.667, 0.000,
+        0.333, 1.000, 0.000,
+        0.667, 0.333, 0.000,
+        0.667, 0.667, 0.000,
+        0.667, 1.000, 0.000,
+        1.000, 0.333, 0.000,
+        1.000, 0.667, 0.000,
+        1.000, 1.000, 0.000,
+        0.000, 0.333, 0.500,
+        0.000, 0.667, 0.500,
+        0.000, 1.000, 0.500,
+        0.333, 0.000, 0.500,
+        0.333, 0.333, 0.500,
+        0.333, 0.667, 0.500,
+        0.333, 1.000, 0.500,
+        0.667, 0.000, 0.500,
+        0.667, 0.333, 0.500,
+        0.667, 0.667, 0.500,
+        0.667, 1.000, 0.500,
+        1.000, 0.000, 0.500,
+        1.000, 0.333, 0.500,
+        1.000, 0.667, 0.500,
+        1.000, 1.000, 0.500,
+        0.000, 0.333, 1.000,
+        0.000, 0.667, 1.000,
+        0.000, 1.000, 1.000,
+        0.333, 0.000, 1.000,
+        0.333, 0.333, 1.000,
+        0.333, 0.667, 1.000,
+        0.333, 1.000, 1.000,
+        0.667, 0.000, 1.000,
+        0.667, 0.333, 1.000,
+        0.667, 0.667, 1.000,
+        0.667, 1.000, 1.000,
+        1.000, 0.000, 1.000,
+        1.000, 0.333, 1.000,
+        1.000, 0.667, 1.000,
+        0.333, 0.000, 0.000,
+        0.500, 0.000, 0.000,
+        0.667, 0.000, 0.000,
+        0.833, 0.000, 0.000,
+        1.000, 0.000, 0.000,
+        0.000, 0.167, 0.000,
+        0.000, 0.333, 0.000,
+        0.000, 0.500, 0.000,
+        0.000, 0.667, 0.000,
+        0.000, 0.833, 0.000,
+        0.000, 1.000, 0.000,
+        0.000, 0.000, 0.167,
+        0.000, 0.000, 0.333,
+        0.000, 0.000, 0.500,
+        0.000, 0.000, 0.667,
+        0.000, 0.000, 0.833,
+        0.000, 0.000, 1.000,
+        0.000, 0.000, 0.000,
+        0.143, 0.143, 0.143,
+        0.286, 0.286, 0.286,
+        0.429, 0.429, 0.429,
+        0.571, 0.571, 0.571,
+        0.714, 0.714, 0.714,
+        0.857, 0.857, 0.857,
+        0.000, 0.447, 0.741,
+        0.314, 0.717, 0.741,
+        0.50, 0.5, 0
+    ]
+).astype(np.float32).reshape(-1, 3)
+
+
+def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
+
+    for i in range(len(boxes)):
+        box = boxes[i]
+        cls_id = int(cls_ids[i])
+        cls_id = min(cls_id, 79) # 
+        score = scores[i]
+        if score < conf:
+            continue
+        x0 = int(box[0])
+        y0 = int(box[1])
+        x1 = int(box[2])
+        y1 = int(box[3])
+
+        color = (_COLORS[cls_id] * 255).astype(np.uint8).tolist()
+        text = '{}:{:.1f}%'.format(class_names[cls_id], score * 100)
+        txt_color = (0, 0, 0) if np.mean(_COLORS[cls_id]) > 0.5 else (255, 255, 255)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
+        cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
+
+        txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
+        cv2.rectangle(
+            img,
+            (x0, y0 + 1),
+            (x0 + txt_size[0] + 1, y0 + int(1.5*txt_size[1])),
+            txt_bk_color,
+            -1
+        )
+        cv2.putText(img, text, (x0, y0 + txt_size[1]), font, 0.4, txt_color, thickness=1)
+
+    return img
+
+
+if __name__ == '__main__':
+    # load model
+    parser = argparse.ArgumentParser(
+        'Deformable DETR training and evaluation script',
+        parents=[get_args_parser()])
+    args = parser.parse_args()
+
+    model, criterion, postprocessors = build_model(args)
+
+    checkpoint = torch.load(args.ckpt, map_location='cpu')
+    model.load_state_dict(checkpoint['model'], strict=False)
+    model.eval()
+    model.cuda()
+    score_thres = 0.3
+    # load dataset
+    video_root_dir = args.video_dir
+    video_data = getFrames(video_root_dir)
+    num_frms = len(video_data)
+    preprocess = ValTransform(legacy=True)
+    postProcess = PostProcess()
+
+    # start to inference
+    for idx in tqdm(range(num_frms)):
+        # get local frames
+        key_frame = video_data[idx]
+        local_frames = [max(0, idx-1), min(num_frms-1, idx+1)]
+        # get global frames:
+        select_range = [0, len(video_data)]
+        select_candidate = list(range(*select_range))
+        select_candidate.remove(idx)
+        for t_idx in local_frames:
+            if t_idx in select_candidate:
+                select_candidate.remove(t_idx)
+
+        global_frames = random.sample(select_candidate, 3)
+
+        local_frames = [video_data[x] for x in local_frames]
+        global_frames = [video_data[x] for x in global_frames]
+
+        frames = [key_frame] + local_frames + global_frames
+        key_img = cv2.imread(key_frame)
+
+        imgs, ori_shape = load_imgs(frames)
+
+        imgs = torch.stack(imgs).unsqueeze(0)
+        imgs = imgs.cuda()
+
+        preds = model(imgs)
+        target_sizes = torch.from_numpy(np.array(list(ori_shape))).unsqueeze(0).cuda()
+        results = postProcess(preds,  target_sizes)[0]
+
+        keeps = (results['scores'] > score_thres)
+        boxes = results['boxes'][keeps].cpu().numpy()
+        cls_ids = results['labels'][keeps].cpu().numpy()
+        scores = results['scores'][keeps].cpu().numpy()
+
+        if len(results['boxes']) == 0 : continue
+
+        img_w_annos = vis(key_img, boxes, scores, cls_ids, conf=0.3, class_names=["", "benign", "malignant"])
+        
+        tmp = key_frame.split("/")
+        video_folder = f"{args.save_dir}/{tmp[-2]}"
+        if not os.path.exists(video_folder):
+            os.makedirs(video_folder)            
+        
+        cv2.imwrite(f"{video_folder}/{tmp[-1]}", img_w_annos)
+        
+        pass
+
+
+
+
