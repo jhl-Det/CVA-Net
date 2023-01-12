44c7c99d63b2f8dae33f3527b26549d95cd57dff
jhl-Det <107089734+jhl-Det@users.noreply.github.com>
2023-01-12 12:48:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,6 @@ def get_args_parser():
                         default='',
                         type=str,
                         help="Name of the shuffled video augmentation to use")
-    parser.add_argument('--umap', default=False, action='store_true')
-    parser.add_argument('--umap_loss', default=False, action='store_true')
 
     parser.add_argument('--lr', default=5e-5, type=float)
     parser.add_argument('--lr_backbone_names',
@@ -335,28 +333,13 @@ def main(args):
                 args.resume, map_location='cpu', check_hash=True)
         else:
             checkpoint = torch.load(args.resume, map_location='cpu')
-        missing_keys, unexpected_keys = model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
+        missing_keys, unexpected_keys = model_without_ddp.load_state_dict(checkpoint['model'], strict=True)
         unexpected_keys = [k for k in unexpected_keys if not (k.endswith('total_params') or k.endswith('total_ops'))]
         if len(missing_keys) > 0:
             print('Missing Keys: {}'.format(missing_keys))
         if len(unexpected_keys) > 0:
             print('Unexpected Keys: {}'.format(unexpected_keys))
         if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
-            # import copy
-            # p_groups = copy.deepcopy(optimizer.param_groups)
-            # optimizer.load_state_dict(checkpoint['optimizer'])
-            # for pg, pg_old in zip(optimizer.param_groups, p_groups):
-            #     pg['lr'] = pg_old['lr']
-            #     pg['initial_lr'] = pg_old['initial_lr']
-            # print(optimizer.param_groups)
-            # lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
-            # todo: this is a hack for doing experiment that resume from checkpoint and also modify lr scheduler (e.g., decrease lr in advance).
-            # args.override_resumed_lr_drop = True
-            # if args.override_resumed_lr_drop:
-            #     print('Warning: (hack) args.override_resumed_lr_drop is set to True, so args.lr_drop would override lr_drop in resumed lr_scheduler.')
-            #     lr_scheduler.step_size = args.lr_drop
-            #     lr_scheduler.base_lrs = list(map(lambda group: group['initial_lr'], optimizer.param_groups))
-            # lr_scheduler.step(lr_scheduler.last_epoch)
             args.start_epoch = checkpoint['epoch'] + 1
         # check the resumed model
         if not args.eval:
